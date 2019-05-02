096c308abd95ae70d8f6fb0a7d1824723083974a
Thomas Jalabert <thom.jalabert@gmail.com>
2019-05-02 21:23:06 +0200

    add auto tag to latest

--- push_img.sh
+++ push_img.sh
@@ -5,3 +5,6 @@ img_id=$(docker images -q base_img)
 
 docker tag $img_id ezako1/docker-python-ml:$tag
 docker push ezako1/docker-python-ml:$tag
+
+docker tag $img_id ezako1/docker-python-ml:latest
+docker push ezako1/docker-python-ml:latest
